3489f7be72d2e63b29566ce7e62932c36ccd629e
AndreaMyshkn <a.trkvsk@hotmail.com>
2019-12-02 13:56:42 -0600

    Sending request without Express

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) =>{
- console.log(req)
+
+ res.setHeader('Content-type', 'text/html')
+ res.write('<html>')
+ res.write('<head><title>hola</title></head>')
+ res.write('<body><h1>Hello</h1></body>')
+ res.write('</html>')
+ res.end()
 });
 
-server.listen(3000)
+server.listen(3002)
