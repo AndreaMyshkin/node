11aefd5b4dc8bcc6bc530c6ae1374bde12f118be
AndreaMyshkn <a.trkvsk@hotmail.com>
2019-12-02 15:35:32 -0600

    Redirecting Requests

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 const http = require('http');
+const fs = require('fs')
 
 const server = http.createServer((req, res) =>{
+  const url = req.url;
+  const method = req.method;
+  if (url ==='/'){
+ 
+    res.write('<html>')
+    res.write('<head><title>Enter message</title></head>')
+    res.write('<body><form action="/message" method="POST"><input type="text" name="message"> <button type="submit">send </button></form></body>')
+    res.write('</html>')
+    return res.end()
+  }
+  if( url ==='/message' && method ==='POST'){
+    fs.writeFileSync('hola.txt', ' Hic et nunc');
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    return res.end()
+}
 
- res.setHeader('Content-type', 'text/html')
- res.write('<html>')
- res.write('<head><title>hola</title></head>')
- res.write('<body><h1>Hello</h1></body>')
- res.write('</html>')
- res.end()
-});
+ 
+  }
+
+);
 
 server.listen(3002)
