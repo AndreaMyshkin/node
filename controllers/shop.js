6b335a1e462e8ea549210857b57a27316388289c
AndreaMyshkn <a.trkvsk@hotmail.com>
2019-12-17 16:14:09 -0600

    Passing Data with POST Requests

--- controllers/shop.js
+++ controllers/shop.js
@@ -13,10 +13,16 @@ exports.getProducts = (req, res, next) => {
 exports.getProduct =(req, res, next) =>{
   const productId = req.params.productId;
 Product.eachProduct(productId,product=>{
- console.log(product)
+ res.render('shop/product-detail', {product:product, pageTitle:product.title, path:'/products'})
 });
 
-  res.redirect('/')
+ 
+}
+
+exports.postProduct = ( req, res, next) =>{
+  const prodId = req.params.productId;
+  console.log(prodId);
+  res.redirect('/cart')
 }
 
 exports.getIndex = (req, res, next) => {
